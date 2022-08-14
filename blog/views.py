c77724608997271ea834598a79eb1f6bc9c4e51a
ggbadza <ggbadza@gmail.com>
2022-08-14 13:00:53 +0900

    CBV test

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+from msilib.schema import ListView
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    model=Post
+    ordering='-pk'
+
+def single_post_page(request,pk):
+    post=Post.objects.get(pk=pk)
+    return render(request,'blog/single_post_page.html',{'post':post})
