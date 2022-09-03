50aeba6a9298e4ab3d41912b85c16e718e48992a
ggbadza <ggbadza@gmail.com>
2022-09-03 14:11:07 +0900

    업로드 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=30)
     content=models.TextField()
     head_image = models.ImageField(upload_to='blog/images/%Y_%m_%d/', blank=True) #폴더 여러개로 구분시켜야지 성능 영향 X, blank는 비어도 된다는 뜻
+    upload_file = models.FileField(upload_to='blog/files/%Y_%m_%d/', blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
@@ -13,3 +15,6 @@ class Post(models.Model):
 
     def get_detail_url(self):
         return f'/blog/{self.pk}'
+
+    def file_name(self):
+        return os.path.basename(self.upload_file.name)
